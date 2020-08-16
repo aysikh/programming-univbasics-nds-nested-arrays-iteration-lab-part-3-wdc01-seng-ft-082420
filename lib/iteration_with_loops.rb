9cc6bb983a0a846b8f8ff175bb22e6ae00a11985
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-16 01:20:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
     strings = " "
     while element_index < src[row_index].count do
       if src[row_index][element_index].is_a? String
-        strings << src[row_index][element_index]
+        array_of_strings << src[row_index][element_index]
       end
       element_index += 1 
     end
